e7d2d2b829a61b2917ee681733e2820fc4631398
gustavlundgren <sgvln005@edu.karlstad.se>
2022-12-09 17:59:37 +0100

    Movement

---
+++ Into the bushes/Assets/Scripts/MoveToMouse.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToMouse : MonoBehaviour
+{
+
+    public float speed = 5f;
+    private Vector3 target;
+    private Camera cam;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = transform.position;
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            target = cam.ScreenToWorldPoint(Input.mousePosition);
+            target.z = transform.position.z;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+    }
+}
---
+++ Into the bushes/Assets/Scripts/followCam.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followCam : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    private Vector3 playerPos;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerPos = Camera.main.WorldToViewportPoint(player.transform.position);
+
+        if (playerPos.x > 0.51)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x + 15, transform.position.y, transform.position.z), speed * Time.deltaTime);
+        }
+
+        if (playerPos.x < 0.49)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x - 15, transform.position.y, transform.position.z), speed * Time.deltaTime);
+        }
+
+        if (playerPos.y > 0.8)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, transform.position.y + 15, transform.position.z), speed * Time.deltaTime);
+        }
+
+        if (playerPos.y < 0.2)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, transform.position.y - 15, transform.position.z), speed * Time.deltaTime);
+        }
+    }
+}
