ed43c4bc085e3cef79b6f0058bafa03dce1a9a1f
gustavlundgren <sgvln005@edu.karlstad.se>
2022-12-09 18:03:40 +0100

    Ring

--- Into the bushes/Assets/Scripts/MoveToMouse.cs
+++ Into the bushes/Assets/Scripts/MoveToMouse.cs
@@ -8,6 +8,7 @@ public class MoveToMouse : MonoBehaviour
     public float speed = 5f;
     private Vector3 target;
     private Camera cam;
+    [SerializeField] GameObject ring;
 
 
     // Start is called before the first frame update
@@ -24,6 +25,7 @@ public class MoveToMouse : MonoBehaviour
         {
             target = cam.ScreenToWorldPoint(Input.mousePosition);
             target.z = transform.position.z;
+            Instantiate(ring, target, Quaternion.identity);
         }
 
         transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
