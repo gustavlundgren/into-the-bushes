69547f2f943ca618e009aa21002f78f1237c210b
gustavlundgren <sgvln005@edu.karlstad.se>
2022-12-11 13:14:06 +0100

    LoadingScreen

---
+++ Into the bushes/Assets/EntryCheck.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static System.Runtime.CompilerServices.RuntimeHelpers;
+
+public class EntryCheck : MonoBehaviour
+{
+    sceneSwith sceneSwither;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sceneSwither = GameObject.FindGameObjectsWithTag("LevelLoader").GetComponenet<sceneSwith>();
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && Input.GetAxis("Submit") == 1)
+        {
+            print("E");
+        }
+    }
+}
--- Into the bushes/Assets/Scripts/followCam.cs
+++ Into the bushes/Assets/Scripts/followCam.cs
@@ -4,39 +4,25 @@ using UnityEngine;
 
 public class followCam : MonoBehaviour
 {
-    [SerializeField] private Transform player;
-    private Vector3 playerPos;
-    public float speed;
+    public float speed = 5f;
+    private Vector3 target;
+    private Camera cam;
 
-    // Start is called before the first frame update
+
     void Start()
     {
-   
+        target = transform.position;
+        cam = Camera.main;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        playerPos = Camera.main.WorldToViewportPoint(player.transform.position);
-
-        if (playerPos.x > 0.51)
+        if (Input.GetMouseButtonDown(0))
         {
-            transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x + 15, transform.position.y, transform.position.z), speed * Time.deltaTime);
+            target = cam.ScreenToWorldPoint(Input.mousePosition);
+            target.z = transform.position.z;
         }
 
-        if (playerPos.x < 0.49)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x - 15, transform.position.y, transform.position.z), speed * Time.deltaTime);
-        }
-
-        if (playerPos.y > 0.7)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, transform.position.y + 15, transform.position.z), speed * Time.deltaTime);
-        }
-
-        if (playerPos.y < 0.69)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, transform.position.y - 15, transform.position.z), speed * Time.deltaTime);
-        }
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
     }
 }
