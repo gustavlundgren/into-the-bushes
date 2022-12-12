ec5655a0e7ba149f9df5a798ea00ecc79ca58618
gustavlundgren <sgvln005@edu.karlstad.se>
2022-12-12 23:10:33 +0100

    Arter grud

---
+++ Into the bushes/Assets/Scripts/Animal_script.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal_script : MonoBehaviour
+{
+    public bool isFlying;
+    public string biom;
+    public float spawnRate;
+    public string type;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Into the bushes/Assets/Scripts/Ring.cs
+++ Into the bushes/Assets/Scripts/Ring.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 
 public class Ring : MonoBehaviour
 {
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
